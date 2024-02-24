a05ec8b62a1add0dabb7ca32d6e7dc69fef33eb6
Juliana Rhee <juliana.rhee@gmail.com>
2024-02-24 17:06:33 -0500

    add funcs to create param dfs

--- dlc.py
+++ dlc.py
@@ -20,10 +20,21 @@ import utils as util
 
 from shapely.geometry import Point, MultiPoint
 #from shapely.geometry.polygon import Polygon
+import scipy.signal as signal
 
 # ---------------------------------------------------------------------
 # general
 # ---------------------------------------------------------------------
+
+def lpfilter(input_signal, win):
+    # Low-pass linear Filter
+    # (2*win)+1 is the size of the window that determines the values that influence 
+    # the filtered result, centred over the current measurement
+    from scipy import ndimage
+    kernel = np.lib.pad(np.linspace(1,3,win), (0,win-1), 'reflect') 
+    kernel = np.divide(kernel,np.sum(kernel)) # normalise
+    output_signal = ndimage.convolve(input_signal, kernel) 
+    return output_signal
 
 def butter_lowpass(data, cutoff, fs, order):
     # implements a butterworth lowpass filter to a signal (data) sampled at fs, with a set cutoff. 
@@ -45,7 +56,82 @@ def get_continuous_numbers(nums):
     return list(zip(edges, edges))
 
 
+# 
+def get_fly_params(flypos, cop_ix=None):
+    if cop_ix is None:
+        cop_ix = len(flypos)
+    fly_ctr = get_animal_centroid(flypos)
+    # get some more female parameters
+    flydf = pd.DataFrame(get_bodypart_angle(flypos.iloc[:cop_ix], 'head', 'thorax'),
+                                columns=['heading'])
+    flydf['centroid_x'] = fly_ctr[:cop_ix, 0]
+    flydf['centroid_y'] = fly_ctr[:cop_ix, 1]
+    flydf['lin_speed'] = np.concatenate(
+                            (np.zeros(1), 
+                            np.sqrt(np.sum(np.square(np.diff(fly_ctr[:cop_ix, ], axis=0)), 
+                            axis=1))))
+    leftw = get_bodypart_angle(flypos, 'thorax', 'wingL')
+    rightw = get_bodypart_angle(flypos, 'thorax', 'wingR')
+    flydf['left_wing_angle'] = wrap2pi(circular_distance(flydf['heading'].interpolate(), leftw)) - np.pi
+    flydf['right_wing_angle'] = wrap2pi(circular_distance(flydf['heading'].interpolate(), rightw)) - np.pi
+    flydf['inter_wing_dist'] = get_bodypart_distance(flypos, 'wingR', 'wingL')
 
+    return flydf
+
+def get_dot_params(dotpos, cop_ix=None):
+    if cop_ix is None:
+        cop_ix = len(dotpos)
+    dot_ctr = get_animal_centroid(dotpos)
+    dotdf = pd.DataFrame(get_bodypart_angle(dotpos.iloc[:cop_ix], 'center', 'top'),
+                                columns=['heading'])
+    dotdf['centroid_x'] = dot_ctr[:cop_ix, 0]
+    dotdf['centroid_y'] = dot_ctr[:cop_ix, 1]
+    dotdf['lin_speed'] = np.concatenate(
+                            (np.zeros(1), 
+                            np.sqrt(np.sum(np.square(np.diff(dot_ctr[:cop_ix, ], axis=0)), 
+                            axis=1))))
+    leftw_d = get_bodypart_angle(dotpos, 'center', 'left')
+    rightw_d = get_bodypart_angle(dotpos, 'center', 'right')
+    dotdf['left_wing_angle'] = wrap2pi(circular_distance(dotdf['heading'].interpolate(), leftw_d)) - np.pi
+    dotdf['right_wing_angle'] = wrap2pi(circular_distance(dotdf['heading'].interpolate(), rightw_d)) - np.pi
+    dotdf['inter_wing_dist'] = get_bodypart_distance(dotpos, 'left', 'right')
+
+    return dotdf
+
+
+def get_interfly_params(flydf, dotdf, cop_ix=None):
+    if cop_ix is None:
+        cop_ix = len(flydf)   
+    # inter-fly stuff
+    # get inter-fly distance based on centroid
+    fly_ctr = flydf[['centroid_x', 'centroid_y']].values 
+    dot_ctr = dotdf[['centroid_x', 'centroid_y']].values 
+
+    IFD = np.sqrt(np.sum(np.square(dot_ctr - fly_ctr),axis=1))
+    flydf['dist_to_other'] = IFD[:cop_ix]
+    flydf['facing_angle'], flydf['ang_between'] = get_relative_orientations(
+                                                            flydf, dotdf)
+    dotdf['dist_to_other'] = IFD[:cop_ix]
+    dotdf['facing_angle'], dotdf['ang_between'] = get_relative_orientations(dotdf, flydf)
+    return flydf, dotdf
+
+
+def load_trk_df(fpath, flyid='fly', fps=60, max_jump=6, cop_ix=None):
+    
+    trk = pd.read_hdf(fpath)
+    tstamp = np.linspace(0, len(trk) * 1 / fps, len(trk))
+    flypos = trk.xs(flyid, level='individuals', axis=1)
+    flypos = remove_jumps(flypos, max_jump)
+    
+    if cop_ix is None:
+        cop_ix = len(flypos)
+    if 'fly' in flyid:
+        flydf = get_fly_params(flypos, cop_ix=cop_ix)
+    else:
+        flydf = get_dot_params(flypos, cop_ix=cop_ix)
+    flydf['time'] = tstamp
+    
+    return flydf
 
 # ---------------------------------------------------------------------
 # measurement
@@ -65,6 +151,7 @@ def get_animal_centroid(dataframe):
     # From Tom/Rufei
     scorer = dataframe.columns.get_level_values(0).unique()[0]
     bptNames = dataframe.columns.get_level_values(1).unique()
+    nFrames = len(dataframe)
     aniXpos, aniYpos = np.zeros((nFrames, len(bptNames[0:8]))), np.zeros((nFrames, len(bptNames[0:8])))
 
     for i, nm in enumerate(bptNames[0:8]):
@@ -99,16 +186,16 @@ def wrap2pi(ang):
     # wraps a set of values to fit between zero and 2Pi (Tom/Rufei)
 
     positiveValues = (ang > 0)
-    wrappedAng = ang % (2 * pi)
-    wrappedAng[ang == 0 & positiveValues] = 2 * pi
+    wrappedAng = ang % (2 * np.pi)
+    wrappedAng[ang == 0 & positiveValues] = 2 * np.pi
 
     return wrappedAng
 
 
-def get_relative_orientations(ani1, ani2):
+def get_relative_orientations(ani1, ani2, xvar='centroid_x', yvar='centroid_y'):
 
-    normPos = ani2[['XCenter', 'YCenter']] - ani1[['XCenter', 'YCenter']]
-    absoluteAngle = np.arctan2(normPos['YCenter'], normPos['XCenter'])
+    normPos = ani2[[xvar, yvar]] - ani1[[xvar, yvar]]
+    absoluteAngle = np.arctan2(normPos[yvar], normPos[xvar])
     fA = circular_distance(absoluteAngle, ani2['heading'])
     aBetween = circular_distance(ani1['heading'],ani2['heading'])
 
@@ -126,12 +213,12 @@ def get_valid_coords(df, pcutoff=0.9):
     return xyp[:, :, :2]
 
 
-def removeJumps(dataframe, maxJumpLength):
+def remove_jumps(dataframe, maxJumpLength):
     # removes large jumps in the x/y position of bodyparts, usually resulting from swaps between animals
 
     # get all column names
     scorer = dataframe.columns.get_level_values(0)[0]
-    bps = list(dataframe.columns.levels[1])
+    bps = list(dataframe.columns.get_level_values(1).unique()) #list(dataframe.columns.levels[1])
     params = list(dataframe.columns.levels[2])
     dataframeMod = dataframe.copy()
 
@@ -190,8 +277,101 @@ def removeJumps(dataframe, maxJumpLength):
 
     return dataframeMod
 
+# GET EPOCHS:
+def smooth_speed_steps(dotdf, win=13, cop_ix=None):
+    if cop_ix is None:
+        cop_ix = len(dotdf)
+    smoothed_x = lpfilter(dotdf['centroid_x'], win)
+    smoothed_y = lpfilter(dotdf['centroid_y'], win)
 
+    dot_ctr_sm = np.dstack([smoothed_x, smoothed_y]).squeeze()
+    # dotdf['lin_speed_filt'] = smoothed_speed
+    dotdf['lin_speed_filt'] = np.concatenate(
+                            (np.zeros(1), 
+                            np.sqrt(np.sum(np.square(np.diff(dot_ctr_sm[:cop_ix, ], axis=0)), 
+                            axis=1)))).round(2)
+    dotdf['centroid_x_filt'] = smoothed_x
+    dotdf['centroid_y_filt'] = smoothed_y
 
+    return dotdf
+
+def get_step_shift_index(dary, find_stepup=True):
+    '''
+    get index of shift in a noisy step function
+    '''
+    dary -= np.average(dary)
+    step = np.hstack((np.ones(len(dary)), -1*np.ones(len(dary))))
+    dary_step = np.convolve(dary, step, mode='valid')
+    # Get the peaks of the convolution
+    # peaks = signal.find_peaks(dary_step, width=1500) #[0]
+    if find_stepup:
+        step_indx = np.argmax(dary_step)  # 
+    else:
+        step_indx = np.argmin(dary_step)  # 
+    # for plotting:
+#     pl.figure()
+#     pl.plot(dary)
+#     pl.plot(dary_step/100)
+#     pl.plot((step_indx, step_indx), (dary_step[step_indx]/100, 0), 'r')
+    return step_indx
+
+# get chunks
+
+def get_step_indices(dotdf, speed_var='lin_speed_filt', t_start=20, 
+                     increment=40, n_levels=10):
+    '''
+    Fix DLC tracked dot trajectories with diffspeeds2.csv
+    Smooths dot positions, finds indices of steps in velocity. Use these indices to divide trajectories into epochs.
+    '''
+    # speed_var = 'lin_speed_filt'
+    # t_start = 20
+    # n_epochs = 9
+    if speed_var not in dotdf.columns:
+        dotdf = smooth_speed_steps(dotdf)
+
+    tmpdf = dotdf.copy() #loc[motion_start_ix:].copy()
+    step_dict={}
+    for i in range(n_levels):
+        t_stop = t_start + increment
+        curr_chunk = tmpdf[ (tmpdf['time']>=t_start) & (tmpdf['time']<=t_stop)].copy().interpolate()
+        #if i==(n_levels-1):
+        find_stepup = i < (n_levels-1)
+        tmp_step_ix = get_step_shift_index(np.array(curr_chunk[speed_var].values),
+                                          find_stepup=find_stepup)
+        step_ix = curr_chunk.iloc[tmp_step_ix].name
+        step_dict.update({i: step_ix})
+        t_start = t_stop 
+    return step_dict
+
+def add_speed_epoch(dotdf, step_dict):
+    '''
+    Use step indices found with get_step_indices() to split speed-varying trajectory df into epochs
+    '''
+    dotdf.loc[:step_dict[0], 'epoch'] = 0
+    #flydf.loc[:step_dict[0], 'epoch'] = 0
+    last_ix = step_dict[0]
+    step_dict_values = list(step_dict.values())
+    for i, v in enumerate(step_dict_values):
+        if v == step_dict_values[-1]:
+            dotdf.loc[last_ix:, 'epoch'] = i+1
+            #flydf.loc[last_ix:, 'epoch'] = i+1
+        else:
+            next_ix = step_dict_values[i+1]
+            dotdf.loc[last_ix:next_ix, 'epoch'] = i+1
+            #flydf.loc[last_ix:next_ix, 'epoch'] = i+1
+        last_ix = next_ix
+    return dotdf
+
+def check_speed_steps(dotdf, step_dict):
+    '''
+    Check speed steps and found indices
+    '''
+    fig, ax = pl.subplots()
+    ax.scatter(dotdf['time'], dotdf['lin_speed_filt'], s=2)
+    for i, v in step_dict.items():
+        dotdf.loc[v]['time']
+        ax.plot(dotdf.loc[v]['time'], dotdf.loc[v]['lin_speed_filt'], 'r*')
+    return fig
 
 # ---------------------------------------------------------------------
 # DEEPLCUT functions
@@ -263,4 +443,25 @@ def plot_skeleton(coords, inds=None, ax=None, color='k', alpha=1, lw=1):
 #         ax.add_collection(col0)
 
 
+## filter
+
+def get_filtered_pos(df, bp, pcutoff=0.9, return_df=True):
+    '''
+    df (pd.DataFrame): multi-level df loaded form .h5 file
+    bp (str): name of bodypart to get coords for
+    '''
+    prob = df.xs(
+        (bp, "likelihood"), level=(-2, -1), axis=1).values.squeeze()
+    mask = prob < pcutoff
+    x_filt = np.ma.array(
+        df.xs((bp, "x"), level=(-2, -1), axis=1).values.squeeze(),
+        mask=mask)
+    y_filt = np.ma.array(
+        df.xs((bp, "y"), level=(-2, -1), axis=1).values.squeeze(),
+        mask=mask)
+    if return_df:
+        df_ = pd.DataFrame({'x': x_filt, 'y': y_filt})
+        return df_
+    else:
+        return x_filt, y_filt
 
