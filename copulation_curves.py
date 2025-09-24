d4bb7f96c7cc50c9ddb8bf027efa35b11d53b053
Juliana Rhee <juliana.rhee@gmail.com>
2025-09-23 21:46:31 -0400

    add some extra plotting for ele

--- copulation_curves.py
+++ copulation_curves.py
@@ -15,14 +15,14 @@ import numpy as np
 import pandas as pd
 
 import seaborn as sns
-import pylab as pl
+import pylab as plt
 
 import utils as util
 import plotting as putil
 
 #%%
-plot_style='dark'
-putil.set_sns_style(plot_style, min_fontsize=18)
+plot_style='white'
+putil.set_sns_style(plot_style, min_fontsize=24)
 bg_color = [0.7]*3 if plot_style=='dark' else 'k'
 
 # %%
@@ -30,16 +30,20 @@ bg_color = [0.7]*3 if plot_style=='dark' else 'k'
 #basedir = '/Users/julianarhee/Dropbox @RU Dropbox/Juliana Rhee/MF-winged-wingless-20mm'
 #basedir = '/Users/julianarhee/Dropbox @RU Dropbox/Juliana Rhee/caitlin_data'
 #experiment = 'multichamber_20mm_winged_v_wingless'
-basedir = '/Users/julianarhee/Dropbox @RU Dropbox/Juliana Rhee/free_behavior_analysis/ht_winged_vs_wingless'
-experiment = '20mm_3x3_elegans_winged_v_wingless'
+###basedir = '/Users/julianarhee/Dropbox @RU Dropbox/Juliana Rhee/free_behavior_analysis/ht_winged_vs_wingless'
+#experiment = '20mm_3x3_elegans_winged_v_wingless'
 
+basedir = '/Volumes/Juliana/free_behavior_analysis/winged_vs_wingless'
+experiment = '20mm_3x3_Dele'
 #experiment = 'winged-wingless'
 
-csv_fpaths = glob.glob(os.path.join(basedir, '*{}.csv'.format(experiment)))
+experiment_dir = os.path.join(basedir, experiment)
+csv_fpaths = glob.glob(os.path.join(experiment_dir, '*.csv'))
 print(csv_fpaths)
 
 
 #%% Set output dirs
+#figdir = os.path.join(basedir, 'figures', 'copulation_curves')
 figdir = os.path.join(basedir, 'figures', 'copulation_curves')
 if not os.path.exists(figdir):
     os.makedirs(figdir)
@@ -54,6 +58,8 @@ if with_food:
     csv_fpath = [c for c in csv_fpaths if 'food' in c][0]
 else:
     csv_fpath = [c for c in csv_fpaths if 'food' not in c][0]
+
+figid = csv_fpath
 
 print(csv_fpath)
 df0 = pd.read_csv(csv_fpath)
@@ -77,7 +83,7 @@ print(df.shape)
 #%%
 cop_palette = {0: [0.3]*3, 1: [0.7]*3}
 # Plot barplot of counts of copulation success by manipulation_male
-fig, ax = pl.subplots()
+fig, ax = plt.subplots()
 sns.countplot(data=df, x='manipulation_male', hue='copulation', ax=ax,
               palette=cop_palette)
 sns.move_legend(ax, loc='upper left', bbox_to_anchor=(1.1, 1), frameon=False)
@@ -96,8 +102,8 @@ ax.set_title("{}: N copulations for courting pairs".format(species),
 putil.label_figure(fig, csv_fpath)
 # Save
 figname = '{}_copulation_counts_{}'.format(species, exp_type)
-pl.savefig(os.path.join(figdir, '{}.png'.format(figname)))
-pl.savefig(os.path.join(figdir, '{}.svg'.format(figname)))
+plt.savefig(os.path.join(figdir, '{}.png'.format(figname)))
+plt.savefig(os.path.join(figdir, '{}.svg'.format(figname)))
 
 print(os.path.join(figdir, '{}_copulation_counts{}.png'.format(species, exp_type)))
 
@@ -112,6 +118,16 @@ df['copulation_onset_sec'] = 0
 df.loc[df['copulation']==1, 'copulation_onset_sec'] = df.loc[df['copulation']==1, 'copulation_onset'].apply(convert_time_to_seconds)
 df['copulation_onset_min'] = df['copulation_onset_sec'] / 60
 
+exclude = ['20250605-0920_fly1-9_Dele-HK_3do_gh']
+df = df[~df['file_name'].isin(exclude)]
+
+df['frontal_behavior_onset'] = df['event_onset'].astype(str)
+df['frontal_behavior_onset_sec'] = 0
+df['frontal_behavior'] = [1 if v=='yes' else 0 for v in df['frontal_behavior_present']]
+df.loc[df['frontal_behavior']==1, 'frontal_behavior_onset_sec'] = df.loc[df['frontal_behavior']==1, 
+                                                                          'frontal_behavior_onset'].apply(convert_time_to_seconds)
+df['frontal_behavior_onset_min'] = df['frontal_behavior_onset_sec'] / 60
+
 # plot experimental cumulative distribution of copulation_onset times
 # for wing
 
@@ -122,15 +138,19 @@ df['copulation_onset_min'] = df['copulation_onset_sec'] / 60
 
 #%%
 
-def cumulative_copulations_by_minute(df_, bins, normalize=False, 
+def cumulative_copulations_by_minute(df_, bins, behavior_var='copulation',
+                                     normalize=False, 
                                      norm_value=None, unit='min'):
-    '''Bin the data by min (or sec) and count N copulations up to that time.'''
+    '''
+    Bin the data by min (or sec) and count N copulations up to that time.
+    Assumes that df has 'copulation' and 'copulation_onset_sec' (or whatever unit).
+    '''
     bins_ = [] 
     cum_ = []
     # For each bin, count the number of copulations that occurred up to that time
     for i, bin in enumerate(bins): #bin_edges):
-        n_in_bin = df_[ (df_['copulation']==1)
-                      & (df_['copulation_onset_{}'.format(unit)]<=bin)].shape[0]
+        n_in_bin = df_[ (df_[behavior_var]==1)
+                      & (df_['{}_onset_{}'.format(behavior_var, unit)]<=bin)].shape[0]
         bins_.append(bin)
         cum_.append(n_in_bin)
     cum_ = np.array(cum_)
@@ -157,35 +177,38 @@ n_wingless_pairs = df[df['manipulation_male']=='wingless'].shape[0]
 label1 = 'winged (n={})'.format(n_winged_pairs)
 label2 = 'wingless (n={})'.format(n_wingless_pairs)
 
+behavior_var = 'frontal_behavior'
 unit = 'min'
 
 bins = np.arange(0, 3600, 60) if unit=='sec' else np.arange(0, 60, 1)
 cum1, _ = cumulative_copulations_by_minute(df[df['manipulation_male']==cond1], 
-                                           bins, normalize=normalize, unit=unit,
+                                           bins, behavior_var=behavior_var,
+                                           normalize=normalize, unit=unit,
                                            norm_value=n_winged_pairs)
 cum2, _ = cumulative_copulations_by_minute(df[df['manipulation_male']==cond2], 
-                                           bins, normalize=normalize, unit=unit,
+                                           bins, behavior_var=behavior_var,
+                                           normalize=normalize, unit=unit,
                                            norm_value=n_wingless_pairs)
 
-fig, ax =pl.subplots()
+fig, ax =plt.subplots()
 ax.step(bins, cum1, label=label1, color=c1, lw=2)
 ax.step(bins, cum2, label=label2, color=c2, lw=2)
 ax.legend(bbox_to_anchor=(1, 0), loc='lower right', frameon=False, fontsize=16)
 ax.set_box_aspect(1)
 ax.set_xlabel('time ({})'.format(unit))
 if normalize:
-    ax.set_ylabel('Normalized copulation count')
+    ax.set_ylabel('Normalized {} count'.format(behavior_var))
 else:
-    ax.set_ylabel('Cum. copulation count')
+    ax.set_ylabel('Cum. {} count'.format(behavior_var))
 ax.set_ylim([-0.01, 0.65])
 sns.despine(offset=4, trim=True)
 
 putil.label_figure(fig, csv_fpath)
 
 # save
-figname = '{}_cumulative_copulations_by_minute_{}'.format(species, exp_type)
-pl.savefig(os.path.join(figdir, '{}.png'.format(figname)))
-pl.savefig(os.path.join(figdir, '{}.svg'.format(figname)))
+figname = '{}_cumulative_{}_by_{}_{}'.format(species, behavior_var, unit, exp_type)
+plt.savefig(os.path.join(figdir, '{}.png'.format(figname)))
+plt.savefig(os.path.join(figdir, '{}.svg'.format(figname)))
 
 
 #%%
@@ -202,28 +225,51 @@ def get_cumulative_dist(sample, bins, normalize=True):
     else:
         return cumulative, bin_edges
 
-#%
-from statsmodels.distributions.empirical_distribution import ECDF
-#
-sample1 = df[df['manipulation_male']=='winged']['copulation_onset_sec'].values
-sample2 = df[df['manipulation_male']=='wingless']['copulation_onset_sec'].values
-x = np.linspace(0, 3600) #min(sample), max(sample))
-#sample = np.hstack((sample1, sample2))
-# fit a cdf
-ecdf1 = ECDF(sample1)
-ecdf2 = ECDF(sample2)
-# plot the cdf
-pl.plot(ecdf1.x, ecdf1.y)
-pl.plot(ecdf2.x, ecdf2.y)
+#%%
 
-# can also try this:
-#fig, ax = pl.subplots()
-#sns.ecdfplot(data=df, hue='manipulation_male', ax=ax, 
-#             x='copulation_onset_sec', stat='proportion')
 
-# Equivalent to:
-#pl.figure()
-#pl.plot(np.sort(sample1), np.linspace(0, 1, len(sample1), endpoint=False))
-#pl.plot(np.sort(sample2), np.linspace(0, 1, len(sample2), endpoint=False))
- 
+# For elegans, check frontal
+
+cop = df[df['copulation']==1].copy()
+
+# Count number of copulations with frontal_behavior_present for winged vs. wingless
+fig, ax = plt.subplots()
+sns.countplot(data=cop, ax=ax,
+              x='manipulation_male', 
+              hue='frontal_behavior_present', 
+              palette='viridis')
+ax.set_xlabel('')
+sns.move_legend(ax, 'upper left', bbox_to_anchor=(1, 1), frameon=False)
+ax.set_box_aspect(1)
+sns.despine(offset=4, trim=True)
+
+for p in ax.patches:
+    ax.annotate('{:.0f}'.format(p.get_height()), 
+                (p.get_x() + p.get_width()/2 - 0.12, 5))
+
+putil.label_figure(fig, figid)
+
+# svae
+figname = 'frontal_behavior_counts'
+plt.savefig(os.path.join(figdir, '{}.png'.format(figname)))
+plt.savefig(os.path.join(figdir, '{}.svg'.format(figname)))
+
+
+#%%
+
+cols = ['file_name', 'fly_num', 'copulation', 'frontal_behavior', 
+        'circling', 'front wing display', 'front wiggle', 
+        'manipulation_male']
+displaydf = cop[cols].copy()
+
+# staack the behaviors, circling, front wiggle, and front display, 
+behavior_list = ['circling', 'front wing display', 'front wiggle']
+id_vars = [c for c in cols if c not in behavior_list]
+disp = displaydf.melt(id_vars=id_vars, var_name='behavior_type', value_name='present')
+
+# plot counts of each for winged vs wingless
+fig, ax = plt.subplots()
+sns.barplot(data=disp, x='behavior_type', y='present', 
+            hue='manipulation_male', ax=ax)
+
 # %%
